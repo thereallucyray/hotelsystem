c97f5435c5dd71bc64f0cc2e3a0ee6ad21deeecd
MistyKurien <mistykurien@gmail.com>
2023-03-30 22:11:48 -0500

    File fixes. Works for everything now.

--- src/main/java/csi3471/group5/db/Database.java
+++ src/main/java/csi3471/group5/db/Database.java
@@ -1,6 +1,9 @@
 package csi3471.group5.db;
 
 import java.io.*;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.stream.Collectors;
 
@@ -8,7 +11,15 @@ public class Database {
     public static <E,T extends DBStore<E,T>> ArrayList<E> getList(DBStore<E,T> store) {
         ArrayList<E> list = new ArrayList<E>();
         //open and read file
-        File file = new File(Database.class.getResource("/db/"+store.getFilename()+".csv").getFile());
+        System.out.println(Database.class.getResource("/db/"+store.getFilename()+".csv").getFile());
+        URL res = Database.class.getResource("/db/"+store.getFilename()+".csv");
+        File file = null;
+        try {
+            file = new File(res.toURI());
+        } catch (URISyntaxException e) {
+            System.out.println("Can't get resource " + store.getFilename() + ".csv");
+            throw new RuntimeException(e);
+        }
 
         BufferedReader reader = null;
         try {
@@ -32,7 +43,14 @@ public class Database {
     }
     public static <E,T extends DBStore<E,T>> void save(DBStore<E,T> store) {
         //open and write file
-        File file = new File(Database.class.getResource("/db/"+store.getFilename()+".csv").getFile());
+        URL res = Database.class.getResource("/db/"+store.getFilename()+".csv");
+        File file = null;
+        try {
+            file = new File(res.toURI());
+        } catch (URISyntaxException e) {
+            System.out.println("Can't get resource " + store.getFilename() + ".csv");
+            throw new RuntimeException(e);
+        }
         PrintWriter writer = null;
         try {
             writer = new PrintWriter(file);
