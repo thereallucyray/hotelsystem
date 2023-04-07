51a3eff3b788bb2511bd560e99ba2caec0828848
Bmin <bminor230@gmail.com>
2023-04-07 14:44:31 -0500

    Style Changes

--- src/main/java/csi3471/group5/gui/ReservationListGUI.java
+++ src/main/java/csi3471/group5/gui/ReservationListGUI.java
@@ -8,6 +8,7 @@ import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 public class ReservationListGUI extends CleverCards {
     public void init() {
@@ -20,10 +21,11 @@ public class ReservationListGUI extends CleverCards {
         panel.setPreferredSize(new Dimension(500, 10000));
         // setup a scroll pane
         JScrollPane scrollPane = new JScrollPane(panel);
-        scrollPane.setPreferredSize(new Dimension(550, 400));
         this.add(scrollPane);
+        ArrayList<Reservation> reservations = new ReservationStore().query().get();
+        panel.setPreferredSize(new Dimension(500, 100 * reservations.size()));
         panel.setBackground(new Color(180,207,201));
-        for(Reservation res : new ReservationStore().query().get()) {
+        for(Reservation res : reservations) {
             panel.add(reservationPanel(res));
             panel.add(Box.createRigidArea(new Dimension(0,5)));
         }
@@ -31,27 +33,30 @@ public class ReservationListGUI extends CleverCards {
 
     JPanel reservationPanel(Reservation res) {
         JPanel panel = new JPanel();
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setPreferredSize(new Dimension(500, 25));
+        buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.X_AXIS));
         panel.setBackground(new Color(230,239,237));
-        panel.setPreferredSize(new Dimension(500, 75));
-        GridLayout gridLayout = new GridLayout();
-        this.setLayout(gridLayout);
+        panel.setPreferredSize(new Dimension(500, 55));
+//        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
         JLabel lab = new JLabel(res.toString());
         if(res.isActive()) {
             lab.setForeground(new Color(0, 0, 0));
             String text = res.getStatus() == Reservation.Status.CHECKED_IN ? "CHECK OUT" : "CHECK IN";
             JButton button = new JButton(text);
-            panel.add(button);
+            buttonPanel.add(button);
             if(res.getStatus() == Reservation.Status.CHECKED_IN) {
                 button.addActionListener(new CheckOutActionListener(res));
             } else {
                 button.addActionListener(new CheckInActionListener(res));
                 JButton button2 = new JButton("CANCEL");
-                panel.add(button2);
+                buttonPanel.add(button2);
                 button2.addActionListener(new CancelActionListener(res));
             }
         } else {
             lab.setForeground(new Color(100, 100, 100));
         }
+        panel.add(buttonPanel);
         panel.add(lab);
 
         return panel;
