a5c24ec525dac23f9a3bc0031b84c9eb33512932
Bmin <bminor230@gmail.com>
2023-05-03 13:50:54 -0500

    Database in a new place now. Also pom works.

--- src/main/java/csi3471/group5/db/Database.java
+++ src/main/java/csi3471/group5/db/Database.java
@@ -1,10 +1,13 @@
 package csi3471.group5.db;
 
 import java.io.*;
+import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.nio.file.FileSystem;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.jar.JarFile;
 import java.util.stream.Collectors;
 
 /**
@@ -22,12 +25,14 @@ public class Database {
         ArrayList<E> list = new ArrayList<E>();
         //open and read file
         System.out.println(Database.class.getResource("/db/"+store.getFilename()+".csv").getFile());
-        URL res = Database.class.getResource("/db/"+store.getFilename()+".csv");
-        File file = null;
+//        URL res = Database.class.getResource("/db/"+store.getFilename()+".csv");
+        File file;
         try {
-            file = new File(res.toURI());
+            file = getFileResource(store.getFilename());
         } catch (URISyntaxException e) {
             System.out.println("Can't get resource " + store.getFilename() + ".csv");
+            throw new RuntimeException(e);
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
 
@@ -53,12 +58,14 @@ public class Database {
      */
     public static <E,T extends DBStore<E,T>> void save(DBStore<E,T> store) {
         //open and write file
-        URL res = Database.class.getResource("/db/"+store.getFilename()+".csv");
-        File file = null;
+//        URL res = Database.class.getResource("/db/"+store.getFilename()+".csv");
+        File file;
         try {
-            file = new File(res.toURI());
+            file = getFileResource(store.getFilename());
         } catch (URISyntaxException e) {
             System.out.println("Can't get resource " + store.getFilename() + ".csv");
+            throw new RuntimeException(e);
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
         PrintWriter writer = null;
@@ -73,5 +80,28 @@ public class Database {
             writer.println(String.join(",", fields));
         }
         writer.close();
+    }
+
+    /**
+     * Gets a resources at /db/<filename>.csv and makes sure it exists at ./.db/<filename>.csv
+     * Once it is there, it returns the file.
+     * @param filename
+     * @return
+     * @throws IOException
+     * @throws URISyntaxException
+     */
+    private static File getFileResource(String filename) throws IOException, URISyntaxException {
+        URI uri = Database.class.getResource("/db/"+filename+".csv").toURI();
+        File actual = new File("./.db/"+filename+".csv");
+        if(!actual.exists()) {
+            actual.getParentFile().mkdirs();
+            actual.createNewFile();
+            InputStream is = Database.class.getResourceAsStream("/db/"+filename+".csv");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+            PrintWriter writer = new PrintWriter(actual);
+            writer.println(reader.lines().collect(Collectors.joining("\n")));
+            writer.close();
+        }
+        return actual;
     }
 }
